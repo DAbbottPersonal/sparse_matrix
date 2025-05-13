6e535468273925f1d91478a93c97a9cb30868eb2
Dale Abbott <DaleAbbott999@gmail.com>
2025-05-12 20:53:49 -0400

    Adding a save and load functionality

--- SparseMatrix.py
+++ SparseMatrix.py
@@ -1,8 +1,43 @@
+import logging
+from pickle import dump, load
+
 from numpy import array, ndarray
 from numpy import where as np_where
 from numpy.typing import ArrayLike
 from pandas import DataFrame
-from scipy.sparse import csr_matrix, hstack, random, vstack
+from pathlib import Path
+from scipy.sparse import csr_matrix, hstack, random, load_npz, save_npz, vstack
+from tarfile import open as tar_open
+
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+)
+
+
+def create_tarfile(output_name: Path | str, source_dir: Path | str):
+    """Creates a tar archive from a directory.
+
+    Args:
+        output_filename: The path to the output tar file.
+        source_dir: The directory to archive.
+    """
+
+    output_name = Path(output_name)
+    source_dir = Path(source_dir)
+    with tar_open(output_name, "w:gz") as tar:
+        tar.add(source_dir, arcname=source_dir.name)
+
+
+def rmdir(path_: Path | str):
+    """Removes a directory and tree files"""
+    for root, dirs, files in path_.walk(top_down=False):
+        for name in files:
+            (root / name).unlink()
+        for name in dirs:
+            (root / name).rmdir()
+    path_.rmdir()
 
 
 class SparseDataFrame:
@@ -11,11 +46,16 @@ class SparseDataFrame:
         data: any = None,
         columns: ArrayLike | None = None,
         indices: ArrayLike | None = None,
+        tar_file: str | Path = None,
     ) -> None:
-        self.__data = data
-        self.__columns = columns
-        self.__indices = indices
-        self.update_dims()
+        if tar_file:
+            self.load(tar_file)
+        else:
+            self.__data = data
+            self.__columns = columns
+            self.__indices = indices
+            self.update_dims()
+        self.HIDDEN_PATH = Path(f".temp_sparse_files_/")
 
     def update_dims(self):
         self.n_cols = self.__data.shape[1]
@@ -163,3 +203,50 @@ class SparseDataFrame:
             index=self.__indices[-n:] if self.__indices else None,
             columns=self.__columns[-n:] if self.__columns else None,
         )
+
+    def save(self, save_path: Path | str, overwrite: bool = False):
+        """Save sparse matrix to a tar file"""
+
+        save_path = Path(save_path)
+        tar_name = (
+            save_path.parent / f"{save_path.name}.tar"
+            if save_path.suffix != "tar"
+            else save_path
+        )
+        assert (
+            overwrite == True or not tar_name.exists()
+        ), f"File {tar_name} already exists! Set overwrite to True to overwrite."
+
+        self.HIDDEN_PATH.mkdir(parents=True, exist_ok=True)
+        logging.info("Saving the data")
+        save_npz((self.HIDDEN_PATH / "data.npz"), self.__data)
+
+        logging.info("Saving the columns and indices")
+        with open((self.HIDDEN_PATH / "columns.names"), "wb") as f:
+            dump(self.__columns, f)
+        with open((self.HIDDEN_PATH / "indices.names"), "wb") as f:
+            dump(self.__indices, f)
+
+        logging.info(f"Compressing to a tar file {tar_name}")
+        create_tarfile(output_name=tar_name, source_dir=self.HIDDEN_PATH)
+        rmdir(self.HIDDEN_PATH)
+        logging.info("Save complete")
+
+    def load(self, load_path: Path | str):
+        """Load sparse matrix from a tar file"""
+        load_path = Path(load_path)
+        self.HIDDEN_PATH = Path(f".temp_sparse_files_/")
+        tar = tar_open(load_path, "r:gz")
+
+        logging.info("Extracting file")
+        tar.extractall(".")
+
+        logging.info("Loading the data")
+        self.__data = load_npz(self.HIDDEN_PATH / "data.npz")
+        logging.info("Loading the columns and indices")
+        with open((self.HIDDEN_PATH / "columns.names"), "rb") as f:
+            self.__columns = load(f)
+        with open((self.HIDDEN_PATH / "indices.names"), "rb") as f:
+            self.__indices = load(f)
+        rmdir(self.HIDDEN_PATH)
+        logging.info("Load complete")
